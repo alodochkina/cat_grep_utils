99e90776d156023c1e495bd8b85ee3b7f969c2b6
koshkara <lodochkina.a@proton.me>
2023-04-07 10:22:06 +0200

    test test

--- test_scripts/cat/test.sh
+++ test_scripts/cat/test.sh
@@ -2,8 +2,9 @@
 
 cd ../cat || exit
 make test 1>output
-cat ./output
-fail_number=$(awk '/Fail test: / {print $3}' < ./output)
+cat output
+fail_number=$(awk '/Fail test: / {print $3}' < output)
+cat "$fail_number"
 if [[ "$fail_number" -ne 0 ]]
 then
   exit 1
