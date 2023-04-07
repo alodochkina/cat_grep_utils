76abf54e9cf93a6d9ab4082c13ea5aee3fe6713f
koshkara <lodochkina.a@proton.me>
2023-04-07 09:45:19 +0200

    test

--- .github/workflows/test_scripts/cat/test.sh
+++ .github/workflows/test_scripts/cat/test.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-cd ./cat || exit
-
+cd ../../../cat || exit
+make test 2>output
+cat ./output
+fail_number=$(awk '/Fail test: / {print $3}' < file)
+if [ "$fail_number" -ne 0 ]
+then
+  exit 1
+  else exit 0
+  fi
